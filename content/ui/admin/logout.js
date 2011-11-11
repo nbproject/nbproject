aedf604911e8943d2315462376847b485ce885e5
Sacha Zyto <sachazyto@gmail.com>
2011-11-10 22:48:20 -0500

    when visiting a file directly, we let the user know if he has credentials that don't let him see that file and offer an easy way to login or go to main page.

---
+++ content/ui/admin/logout.js
@@ -0,0 +1,28 @@
+/*
+ * logout
+ *
+ Author 
+ Sacha Zyto (sacha@csail.mit.edu) 
+
+ License
+ Copyright (c) 2010 Massachusetts Institute of Technology.
+ MIT License (cf. MIT-LICENSE.txt or http://www.opensource.org/licenses/mit-license.php)
+
+*/
+
+try{    
+    Module.require("NB", 0.1);
+    Module.require("NB.auth", 0.1);
+    Module.require("NB.pers", 0.1);
+
+}
+catch (e){
+    alert("[inbox] Init Error: "+e);
+}
+
+NB.pers.init = function(){
+    var nextpage = NB.pers.params.next;
+    if (nextpage){
+	document.location ="http://"+document.location.host+nextpage;
+    }
+};
