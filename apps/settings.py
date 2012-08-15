bde1eb58e74caa6af33335e8ef9f99339fd7a754
Sacha Zyto <sachazyto@gmail.com>
2012-08-15 00:08:54 -0400

    removing extra context processors

--- apps/settings.py
+++ apps/settings.py
@@ -108,12 +108,8 @@ TEMPLATE_DIRS = (
 ALLOWED_INCLUDE_ROOT = (abspath("%s/../templates/web" % (ROOTDIR, )),)
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-   'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
     'django.core.context_processors.static',
 "django.contrib.auth.context_processors.auth",
-    'django.contrib.messages.context_processors.messages',
 'django_facebook.context_processors.facebook',
 )
 
