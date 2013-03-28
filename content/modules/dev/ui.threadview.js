47bfaa3231606b614d6c5a7e992c9d54716e6b8a
Sacha Zyto <sachazyto@gmail.com>
2013-03-28 11:18:01 -0400

    hid author name

--- content/modules/dev/ui.threadview.js
+++ content/modules/dev/ui.threadview.js
@@ -109,7 +109,7 @@
         else if (o.type === 2){
             type_info        = " <div class='nbicon stafficon' title='[staff] This comment is for Instructors and TAs'/> ";
         }            
-        var author_name        = " <span class='author'>"+o.fullname+"</span> ";
+        var author_name        = " <span class='author'>"+"[name hidden]"+"</span> ";
         var creation_info = " <span class='created'> &ndash; " + (new Date(o.created * 1000)).toPrettyString() + "</span> ";
         var replymenu        = "<a class='replymenu' href='javascript:void(0)'><div class='nbicon replyicon' title='Reply' /></a>";
         var optionmenu        = " <a class='optionmenu' href='javascript:void(0)'><div title='Actions'>&sdot;&sdot;&sdot;</div></a> ";
