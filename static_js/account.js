30b5d2b8c9f0c874254354551d41aec69e6acee2
neastin <neastin@mit.edu>
2013-04-17 14:44:53 -0400

    object mdoel finished

--- static_js/account.js
+++ static_js/account.js
@@ -2,6 +2,7 @@ function account(id) {
 	this.id = id;
 	this.members = new Array();
 	this.tasks= new Array();
+	this.finishedTasks = new Array();
 
 	this.createMember = function(name) {
 		if ( containsNameValueInList(name, this.members) === -1) {
@@ -21,45 +22,50 @@ function account(id) {
 		}
 	}
 
-	this.assignTask = function(task_name, member_name) {
-		if (containsNameValueInList(member_name, this.members) === -1) {
-			console.log("this member does not exist");
+
+	this.finishTask = function(task_name, member_name) {
+		if (containsNameValueInList(task_name, this.tasks) === -1) {
+			console.log("this task does not exist");
 			return;
 		}
 
-		if (containsNameValueInList(task_name, this.tasks) === -1) {
-			console.log("this task does not exist");
+		if (containsNameValueInList(member_name, this.members) === -1) {
+			console.log("this member does not exist");
 			return;
 		}
 
 		var tmp_member = this.members[containsNameValueInList(member_name, this.members)];
 		var tmp_task = this.tasks[containsNameValueInList(task_name, this.tasks)];
 
-		if (tmp_task.assignedTo === undefined) {
-
+		if (tmp_task.finished === undefined) {
+			tmp_task.finished = member_name;
+			tmp_member.completedTasks.push(tmp_task);
+			this.finishedTasks.push(tmp_task);
+			this.tasks.splice( containsNameValueInList(task_name, this.tasks), 1);
+			console.log("task : " + task_name + " successfully completed by member " + member_name);
 		} else {
-			console.log("task is already assigned to " + tmp_task.assignedTo);
+			console.log("task already done");
 			return;
-		}
-		if ( !tmp_member.containsTask(tmp_task)) {
-			tmp_member.assignedTasks.push(tmp_task);
-			tmp_task.assignedTo = member_name;
-			console.log("successfully added task " + task_name + " to member " + member_name);
-		} else {
-			console.log(member_name + " already contains task " + task_name);
 		}
 	}
 
-	this.finishTask = function(task_name, member) {
+	//gets
+
+	this.getMember = function(member_name) {
+		if (containsNameValueInList(member_name, this.members) === -1) {
+			console.log("this member does not exist");
+			return;
+		}
+		return this.members[containsNameValueInList(member_name, this.members)];
+	}
+
+	this.getTask = function(task_name) {
 		if (containsNameValueInList(task_name, this.tasks) === -1) {
 			console.log("this task does not exist");
 			return;
 		}
-
-		var tmp_task = this.tasks[containsNameValueInList(task_name, this.tasks)];
-
-		//if (tmp_task)
-
+		
+		return this.tasks[containsNameValueInList(task_name, this.tasks)];
 
 	}
 }
