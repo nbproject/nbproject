63193b9c6e23600f7c697bf86a38bae0e4a16972
neastin <neastin@mit.edu>
2013-04-17 15:43:53 -0400

    pictures showing now

--- static_js/task.js
+++ static_js/task.js
@@ -3,6 +3,8 @@ function task(name) {
 	this.name = name;
 	//this.duedate = duedate;
 	//this.assignedTo = undefined;
-	this.finished = undefined;
+	this.finished = undefined; // this is the name of member who finished it
+	this.duedate;
+	this.finishdate;
 }
 
