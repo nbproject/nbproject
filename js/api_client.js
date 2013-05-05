fe15c013d9c7b280b8b9b502afbe22954ed754a2
Rafael Mendiola <rafael.mendiola@gengo.com>
2013-05-05 15:07:22 -0400

    Created api client skeleton, started node.js server

---
+++ js/api_client.js
@@ -0,0 +1,45 @@
+function apiClient() {
+
+	this.url = "url";
+
+	//Family Functions
+	this.createAccount = function(account){
+
+	};
+
+	this.getAccount = function(id){
+
+	};
+
+	this.modifyAccount = function(account){
+
+	};
+
+	//Member functions
+	this.createMember = function(member){
+
+	};
+
+	this.getMember = function(id){
+
+	};
+
+	this.modifyMember = function(member){
+
+	};
+
+	//Task functions
+	this.createTask = function(task){
+
+	};
+
+	this.getTask = function(id){
+
+	};
+
+	this.modifyTask = function(task){
+
+	};
+
+
+}
