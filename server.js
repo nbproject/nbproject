0f9c476b0a63fa65afbc4960deb4ab6a5f796762
Rafael Mendiola <rafael.mendiola@gengo.com>
2013-05-05 16:01:53 -0400

    Switched server to express, have abilty to deliver static pages now

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.use(express.static(__dirname));
+
+app.get('/hello', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 8008;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+
+/*
 var http = require('http');
 var port = process.env.PORT || 5000;
 http.createServer(function (req, res) {
@@ -6,3 +23,4 @@ http.createServer(function (req, res) {
 }).listen(port);
 
 console.log('Server running on port ' + port);
+*/
