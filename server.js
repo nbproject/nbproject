87c6929d388004775c5a6adf12acc361ffdb7b9e
Rafael Mendiola <rafael.mendiola@gengo.com>
2013-05-05 15:45:27 -0400

    Modified port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var port = 80;
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(port);
+
+console.log('Server running on port ' + port);
