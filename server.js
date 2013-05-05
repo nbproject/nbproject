18ae34afaf69afe18b5e9462120d53df402be391
Rafael Mendiola <rafael.mendiola@gengo.com>
2013-05-05 15:48:56 -0400

    changed port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var port = 80;
+var port = 5000;
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello World\n');
