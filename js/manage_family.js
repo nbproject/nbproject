ea0f3f7a359ee0bf2fee918fc439d8eb5dee4a10
neastin <neastin@mit.edu>
2013-05-08 03:50:12 -0400

    added much needed name duplication prevention on manage page

--- js/manage_family.js
+++ js/manage_family.js
@@ -2,7 +2,8 @@
 
 $(document).ready(function() {
     var family = getFamilyObject();
-    
+    var familyNames = new Array();
+
     client.getFamilyInfo(family._id, function(data){
         var datamembers = data.members;
         var familyList = $("#family");
@@ -11,7 +12,8 @@ $(document).ready(function() {
         for (var i = 0; i < datamembers.length; i++) {
             var member = datamembers[i];
             var name = member.name;
-
+            console.log(name);
+            familyNames.push(name);
             var pic = member.pictureURL;
             if (pic === "") {
                 pic = "/assets/unknown_person.jpg";
@@ -55,7 +57,7 @@ $(document).ready(function() {
 
     var newMemberVisible = false;
         
-
+    console.log(familyNames);
     function newMemberCallback(data) {
         if(data.error == null){
             var id = data._id;
@@ -69,17 +71,28 @@ $(document).ready(function() {
             var userTableInfo = '<tr><td class="left"><img id="managePic" src="' + pic + '" /></td> <td class="middle">' + name + '</td> <td class="right"><button class="btn btn-danger btn-mini">Delete</btn></td></tr>'
             $(familyTable).append(userTableInfo);
             newMemberVisible = false;
-
+            if (errorAddMember) {
+                errorAddMember = false;
+            }
         }else{
             console.log(data.error);
         }
     }
 
+    var errorAddMember = false;
     function addMember() {
         console.log("--submit Member--");
         var name =  $("#newName").val();
         var pictureURL =  "";
         var member = {'name': name, 'pictureURL':pictureURL};
+        if (familyNames.indexOf(name) != -1) {
+            if (errorAddMember) { return;}
+            $("#familyTable tr:first").append("<td id='error'>This member name already exists!</td>");
+            errorAddMember = true;
+            return;
+        }
+
+
         client.createMember(member, newMemberCallback);
     }
         
