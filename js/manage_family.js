0cda55a49aff7cf9f472eef92587f53da19f3313
Louis <loulamia@mit.edu>
2013-05-08 12:17:16 -0400

    File Upload Test File

--- js/manage_family.js
+++ js/manage_family.js
@@ -2,23 +2,61 @@
 
 $(document).ready(function() {
     var family = getFamilyObject();
-    
+
+    $("#fam").text(family.name);
+
+    var familyNames = new Array();
+    var deleteUserHTML = "";
+    var targetParent;
     client.getFamilyInfo(family._id, function(data){
         var datamembers = data.members;
         var familyList = $("#family");
+
+        var familyTable = $("#familyTable");
         for (var i = 0; i < datamembers.length; i++) {
             var member = datamembers[i];
             var name = member.name;
-
+            console.log(name);
+            familyNames.push(name);
             var pic = member.pictureURL;
-            console.log(pic);
+            if (pic === "") {
+                pic = "/assets/unknown_person.jpg";
+            }
             var userInfo = '<li><div class="profile" id="box' + name + '"><img src="'+ pic + '" id="pic' + name + '"><h3>' + name + '</h3></div></li>'
             
+            var userTableInfo = '<tr id="row'+name+'"><td class="left"><img id="managePic" src="' + pic + '" /></td> <td class="middle">' + name + '</td> <td id=class="right"><button id="delete' + name + '" class="btn btn-danger btn-mini">Delete</btn></td></tr>'
             //var y = '<li><div class="profile" id="box' + name + '"><img src="assets/' + name + '.jpg" id="pic' + name + '"><h3>' + name + '</h3></div></li>'
             $(userInfo).appendTo(familyList);
+            $(familyTable).append(userTableInfo);
             $('.profile').click(function(evt) {
                 window.location = 'member_history.html?member=' + evt.target.id.slice(3);
             });
+
+            $("#delete"+name).click(function(e) {
+                e.stopPropagation();
+                var name = $(this).attr("id").substr(6);
+                var proceed = confirm("Are you sure you want to delete " + name + "?");
+                if (proceed) {
+                    $("#row"+name).remove();
+                    var memberToDelete;
+                    client.getFamilyInfo(family._id, function(data){
+                        for (var i = 0; i < datamembers.length; i++) {
+                            var thismember = datamembers[i];
+                            var thisname = thismember.name;
+                            if (name === thisname) {
+                                console.log("fuck");
+                                memberToDelete = thismember;
+                                client.deleteMember(memberToDelete, function(){});
+
+                                return;
+                            }
+                        }
+                    });
+                }
+
+            });
+
+
         };
 
     });
@@ -32,11 +70,9 @@ $(document).ready(function() {
     	window.location = 'index.html';
     });
     
-    $('#manageFamily').click(function(evt) {
-        window.location = 'manage_family.html';
-    });
+   
 
-    $('#titleButton').click(function(evt) {
+    $('#boxTodo').click(function(evt) {
     	window.location = 'index.html';
     });
 
@@ -44,4 +80,87 @@ $(document).ready(function() {
     $('.profile').click(function(evt) {
         window.location = 'member_history.html?member=' + evt.target.id.slice(3);
     });
+
+
+
+    var newMemberVisible = false;
+        
+    console.log(familyNames);
+    function newMemberCallback(data) {
+        if(data.error == null){
+            var id = data._id;
+            $("#familyTable tr:first").remove();
+            var name = data.name;
+            var pic = data.pictureURL;
+            if (pic === "") {
+                pic = "/assets/unknown_person.jpg";
+            }
+            var familyTable = $("#familyTable");
+            var userTableInfo = '<tr id="row'+name+'"><td class="left"><img id="managePic" src="' + pic + '" /></td> <td class="middle">' + name + '</td> <td id=class="right"><button id="delete' + name + '" class="btn btn-danger btn-mini">Delete</btn></td></tr>'
+            $(familyTable).append(userTableInfo);
+
+            $("#delete"+name).click(function() {
+                e.stopPropagation();
+                var name = $(this).attr("id").substr(6);
+                console.log(name);
+                var proceed = confirm("Are you sure you want to delete " + name + "?");
+
+                if (proceed) {
+                    $("#row"+name).remove();
+                    var memberToDelete;
+                    client.getFamilyInfo(family._id, function(data){
+                        var datamembers = data.members;
+                        for (var i = 0; i < datamembers.length; i++) {
+                            var thismember = datamembers[i];
+                            var thisname = thismember.name;
+                            console.log(name);
+                            if (name === thisname) {
+                                memberToDelete = thismember;
+                                client.deleteMember(memberToDelete, function(){});
+                                return;
+                            }
+                        }
+                    });
+                }
+
+            });
+            newMemberVisible = false;
+            if (errorAddMember) {
+                errorAddMember = false;
+            }
+        }else{
+            console.log(data.error);
+        }
+    }
+
+    var errorAddMember = false;
+    function addMember(e) {
+        //e.stopPropagation();
+        console.log("--submit Member--");
+        var name =  $("#newName").val();
+        var pictureURL =  "";
+        var member = {'name': name, 'pictureURL':pictureURL};
+        if (familyNames.indexOf(name) != -1) {
+            if (errorAddMember) { return;}
+            $("#familyTable tr:first").append("<td id='error'>This member name already exists!</td>");
+            errorAddMember = true;
+            return;
+        }
+
+
+        client.createMember(member, newMemberCallback);
+    }
+        
+    function populatePage(family) {
+        
+    }
+        
+    $("#addMember").click(function(e) {
+        e.stopPropagation();
+        if (!newMemberVisible) {        
+            $("#familyMemberList").prepend('<tr><td class="left"><img id="newPic" src="assets/unknown_person.jpg" /></td> <td class="middle"><input id="newName" type="text"></td> <td class="right"><button id="newAddButton" class="btn btn-success btn-mini">Add</btn></td></tr>');
+            newMemberVisible = true;        
+            $("#newAddButton").click(addMember);        
+        }
+    });         
 });
--- js/member_history.js
+++ js/member_history.js
@@ -15,15 +15,47 @@ $.extend({
     }
 });
 
+
+function doIOwnThis(id, task) {
+    if (task.finished != "") {
+        if (task.finished === id) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (task.assignedTo != "") {
+        if (task.assignedTo === id) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return false;
+}
+
 $(document).ready(function() {
+    var member_name;
+    var profile_member;
+    if ($.getUrlVar('member')) {
+        member_name = $.getUrlVar('member');
+    } else { //? default?
+        window.location = "index.html";
+    }
     var family = getFamilyObject();
     
+    $("#fam").text(family.name);
+
     client.getFamilyInfo(family._id, function(data){
         var datamembers = data.members;
         var familyList = $("#family");
         for (var i = 0; i < datamembers.length; i++) {
             var member = datamembers[i];
             var name = member.name;
+            if (name === member_name) {
+                profile_member = member;
+            }
 
             var pic = member.pictureURL;
             if (pic === "") {
@@ -31,54 +63,81 @@ $(document).ready(function() {
             }
 
 
-            var userInfo = '<li><div class="profile" id="box' + name + '"><img src="'+ pic + '" id="pic' + name + '"><h3>' + name + '</h3></div></li>'
+            var userInfo = '<li><div class="profile '+name+'" id="box' + name + '"><img src="'+ pic + '" id="pic' + name + '" class="'+name +'"><h3>' + name + '</h3></div></li>'
             
             //var y = '<li><div class="profile" id="box' + name + '"><img src="assets/' + name + '.jpg" id="pic' + name + '"><h3>' + name + '</h3></div></li>'
             $(userInfo).appendTo(familyList);
             $('.profile').click(function(evt) {
                 window.location = 'member_history.html?member=' + evt.target.id.slice(3);
             });
+
+            $("div."+member_name).addClass('active');
         };
+
+        var memberTasks = new Array();
+        var datatasks = data.tasks;
+        for (var i = 0; i < datatasks.length; i++) {
+            var curDataTask = datatasks[i];
+            console.log(doIOwnThis( profile_member._id, curDataTask));
+            if (doIOwnThis(profile_member._id, curDataTask)) {
+                memberTasks.push(curDataTask);
+            }
+        };  
+
+
+        var taskHTML = "";
+        for (var i = 0; i < memberTasks.length; i++) {
+            var ct = memberTasks[i];
+            var curHTML = "<div class='task'> <table><tr><td>Task: " + ct.name + "</td></tr> <tr><td>Status: " + ct.status + "</td></tr>"
+            if (ct.status === "Completed") {
+                if (ct.img === undefined) {
+
+                } else {
+                    curHTML += "<tr><td><img src='assets/" + ct.img + "' height ='200'></td></tr>"
+                }
+            }
+            curHTML += "</table> </div>";
+            taskHTML+=curHTML;
+        }
+
+        if (memberTasks.length === 0) {
+            taskHTML = "This family member has no activity!";
+        }
+        
+        $("#memberTasks").html(taskHTML);
 
     });
 
-    if ($.getUrlVar('member')) {
-        member = test_account.getMember($.getUrlVar('member'));
-        member = test_account.getMember("Cersei");
-    } else { //? default?
-        member = test_account.getMember("Cersei");
-    }  
     
 
-
-
-    $("#header").text("Today " + member.name + "...");
+    $("#header").text("Today " + member_name + "...");
     //var imgHTML = "<img src='assets/" + member.name + ".jpg' height='200'>"
     //$("#containsImage").html(imgHTML);
     $("#hideme").hide();
     
     // Color Tabs
-    $('#box'+member.name).css('background', '#C9E9FF');
+    $('#box'+member_name).css('background', '#C9E9FF');
     $('#tabs li').css('background', '#FFFFC9');
     
     $('.profile').click(function(evt) {
     	window.location = window.location.pathname + '?member=' + evt.target.id.slice(3);
     });
     
-    $('#tabTodo').click(function(evt) {
-    	window.location = 'index.html';
+    $('#boxTodo').click(function(evt) {
+        window.location = 'index.html';
     });
     
-     $('#manageFamily').click(function(evt) {
-        window.location = 'manage_family.html';
-    });
+     
 
-    $('#titleButton').click(function(evt) {
-    	window.location = 'index.html';
-    });
+    //$('#titleButton').click(function(evt) {
+    //	window.location = 'index.html';
+    //});
 
     
 
+
+
+/*
     var taskHTML = "";
     for (var i = 0; i < member.tasks.length; i++) {
         var ct = member.tasks[i];
@@ -109,8 +168,11 @@ $(document).ready(function() {
         taskHTML+=curHTML;
     }
 
-    if (member.tasks.length + member.completedTasks.length === 0) {
-        taskHTML = "This family member has no activity!";
-    }
-    $("#memberTasks").html(taskHTML);
+    */
+
+    //if (member.tasks.length + member.completedTasks.length === 0) {
+    //    taskHTML = "This family member has no activity!";
+    //}
+
+    //$("#memberTasks").html(taskHTML);
 });
