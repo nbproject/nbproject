21894b37c090d9b47e42512a47e78dca4b90ec19
neastin <neastin@mit.edu>
2013-05-08 02:19:26 -0400

    users dynamic

--- js/index.js
+++ js/index.js
@@ -7,16 +7,34 @@ $(document).ready(function(){
 	
 	client.getFamilyInfo(family._id, function(data){
 		var datatasks = data.tasks;
+		console.log(data);
 		for (var i = 0; i < datatasks.length; i++) {
 			var curDataTask = datatasks[i];
-			console.log(curDataTask);
 			var task = new Task(curDataTask.name, null,TaskListId);
 			task.duedate = curDataTask.duedate;
 			tasks[TaskListId.toString()] = task;
 			updateTaskList(task);
 			++TaskListId;
 		};			
+
+		var datamembers = data.members;
+		var familyList = $("#family");
+		for (var i = 0; i < datamembers.length; i++) {
+			var member = datamembers[i];
+			var name = member.name;
+
+			var pic = member.pictureURL;
+			console.log(pic);
+			var userInfo = '<li><div class="profile" id="box' + name + '"><img src="'+ pic + '" id="pic' + name + '"><h3>' + name + '</h3></div></li>'
+			
+			//var y = '<li><div class="profile" id="box' + name + '"><img src="assets/' + name + '.jpg" id="pic' + name + '"><h3>' + name + '</h3></div></li>'
+			$(userInfo).appendTo(familyList);
+		};
+
 	});
 
+
+
+	// populate the images
 
 });
