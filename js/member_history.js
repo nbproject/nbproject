1b43c43d06f419cc0ac8030d1ca5d83ac94765be
neastin <neastin@mit.edu>
2013-05-08 05:56:09 -0400

    profile page somewhat working

--- js/member_history.js
+++ js/member_history.js
@@ -15,15 +15,47 @@ $.extend({
     }
 });
 
+
+function doIOwnThis(id, task) {
+    if (task.finished != "") {
+        if (task.finished === id) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (task.assignedTo != "") {
+        if (task.assignedTo === id) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return false;
+}
+
 $(document).ready(function() {
+    var member_name;
+    var profile_member;
+    if ($.getUrlVar('member')) {
+        member_name = $.getUrlVar('member');
+    } else { //? default?
+        window.location = "index.html";
+    }
     var family = getFamilyObject();
     
+
+
     client.getFamilyInfo(family._id, function(data){
         var datamembers = data.members;
         var familyList = $("#family");
         for (var i = 0; i < datamembers.length; i++) {
             var member = datamembers[i];
             var name = member.name;
+            if (name === member_name) {
+                profile_member = member;
+            }
 
             var pic = member.pictureURL;
             if (pic === "") {
@@ -40,25 +72,44 @@ $(document).ready(function() {
             });
         };
 
+        var memberTasks = new Array();
+        var datatasks = data.tasks;
+        for (var i = 0; i < datatasks.length; i++) {
+            var curDataTask = datatasks[i];
+            console.log(doIOwnThis( profile_member._id, curDataTask));
+            if (doIOwnThis(profile_member._id, curDataTask)) {
+                memberTasks.push(curDataTask);
+            }
+        };  
+
+        
+        var taskHTML = "";
+        for (var i = 0; i < memberTasks.length; i++) {
+            var ct = memberTasks[i];
+            var curHTML = "<div class='task'> <table><tr><td>Task: " + ct.name + "</td></tr> <tr><td>Status: " + ct.status + "</td></tr>"
+            if (ct.status === "Completed") {
+                if (ct.img === undefined) {
+
+                } else {
+                    curHTML += "<tr><td><img src='assets/" + ct.img + "' height ='200'></td></tr>"
+                }
+            }
+            curHTML += "</table> </div>";
+            taskHTML+=curHTML;
+        }
+        $("#memberTasks").html(taskHTML);
+
     });
 
-    if ($.getUrlVar('member')) {
-        member = test_account.getMember($.getUrlVar('member'));
-        member = test_account.getMember("Cersei");
-    } else { //? default?
-        member = test_account.getMember("Cersei");
-    }  
     
 
-
-
-    $("#header").text("Today " + member.name + "...");
+    $("#header").text("Today " + member_name + "...");
     //var imgHTML = "<img src='assets/" + member.name + ".jpg' height='200'>"
     //$("#containsImage").html(imgHTML);
     $("#hideme").hide();
     
     // Color Tabs
-    $('#box'+member.name).css('background', '#C9E9FF');
+    $('#box'+member_name).css('background', '#C9E9FF');
     $('#tabs li').css('background', '#FFFFC9');
     
     $('.profile').click(function(evt) {
@@ -79,6 +130,9 @@ $(document).ready(function() {
 
     
 
+
+
+/*
     var taskHTML = "";
     for (var i = 0; i < member.tasks.length; i++) {
         var ct = member.tasks[i];
@@ -109,8 +163,11 @@ $(document).ready(function() {
         taskHTML+=curHTML;
     }
 
-    if (member.tasks.length + member.completedTasks.length === 0) {
-        taskHTML = "This family member has no activity!";
-    }
-    $("#memberTasks").html(taskHTML);
+    */
+
+    //if (member.tasks.length + member.completedTasks.length === 0) {
+    //    taskHTML = "This family member has no activity!";
+    //}
+
+    //$("#memberTasks").html(taskHTML);
 });
