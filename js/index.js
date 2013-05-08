aed18be4ca968258862a17a070d85b7b9cf91e2d
Louis <loulamia@mit.edu>
2013-05-08 03:03:59 -0400

    Uploaded Jquery file upload

---
+++ js/index.js
@@ -0,0 +1,43 @@
+function dum(data) {
+	console.log("success");
+}
+
+$(document).ready(function(){
+	var family = getFamilyObject();
+	
+	client.getFamilyInfo(family._id, function(data){
+		var datatasks = data.tasks;
+		console.log(data);
+		for (var i = 0; i < datatasks.length; i++) {
+			var curDataTask = datatasks[i];
+			var task = new Task(curDataTask.name, null,TaskListId);
+			task.duedate = curDataTask.duedate;
+			tasks[TaskListId.toString()] = task;
+			updateTaskList(task);
+			++TaskListId;
+		};			
+
+		var datamembers = data.members;
+		var familyList = $("#family");
+		for (var i = 0; i < datamembers.length; i++) {
+			var member = datamembers[i];
+			var name = member.name;
+
+			var pic = member.pictureURL;
+            if (pic === "") {
+                pic = "/assets/unknown_person.jpg";
+            }
+			var userInfo = '<li><div class="profile" id="box' + name + '"><img src="'+ pic + '" id="pic' + name + '"><h3>' + name + '</h3></div></li>'
+			
+			//var y = '<li><div class="profile" id="box' + name + '"><img src="assets/' + name + '.jpg" id="pic' + name + '"><h3>' + name + '</h3></div></li>'
+			$(userInfo).appendTo(familyList);
+
+			$('.profile').click(function(evt) {
+                window.location = 'member_history.html?member=' + evt.target.id.slice(3);
+            });
+		};
+
+	});
+
+
+});
