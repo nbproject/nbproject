f5b82ef1097a2bfd13d1ce0dda58547a6af56f39
neastin <neastin@mit.edu>
2013-05-08 03:36:01 -0400

    dynamic manage page

--- js/manage_family.js
+++ js/manage_family.js
@@ -6,16 +6,22 @@ $(document).ready(function() {
     client.getFamilyInfo(family._id, function(data){
         var datamembers = data.members;
         var familyList = $("#family");
+
+        var familyTable = $("#familyTable");
         for (var i = 0; i < datamembers.length; i++) {
             var member = datamembers[i];
             var name = member.name;
 
             var pic = member.pictureURL;
-            console.log(pic);
+            if (pic === "") {
+                pic = "/assets/unknown_person.jpg";
+            }
             var userInfo = '<li><div class="profile" id="box' + name + '"><img src="'+ pic + '" id="pic' + name + '"><h3>' + name + '</h3></div></li>'
             
+            var userTableInfo = '<tr><td class="left"><img id="managePic" src="' + pic + '" /></td> <td class="middle">' + name + '</td> <td class="right"><button class="btn btn-danger btn-mini">Delete</btn></td></tr>'
             //var y = '<li><div class="profile" id="box' + name + '"><img src="assets/' + name + '.jpg" id="pic' + name + '"><h3>' + name + '</h3></div></li>'
             $(userInfo).appendTo(familyList);
+            $(familyTable).append(userTableInfo);
             $('.profile').click(function(evt) {
                 window.location = 'member_history.html?member=' + evt.target.id.slice(3);
             });
@@ -44,4 +50,46 @@ $(document).ready(function() {
     $('.profile').click(function(evt) {
         window.location = 'member_history.html?member=' + evt.target.id.slice(3);
     });
+
+
+
+    var newMemberVisible = false;
+        
+
+    function newMemberCallback(data) {
+        if(data.error == null){
+            console.log("posted member");
+            console.log(data);
+            var id = data._id;
+            console.log(id);
+            client.getMember(id, function(data){
+                console.log("retrieved member");
+                console.log(data);
+            });
+            $("#familyTable tr:first").remove();
+
+        }else{
+            console.log(data.error);
+        }
+    }
+
+    function addMember() {
+        console.log("--submit Member--");
+        var name =  $("#newName").val();
+        var pictureURL =  "";
+        var member = {'name': name, 'pictureURL':pictureURL};
+        client.createMember(member, newMemberCallback);
+    }
+        
+    function populatePage(family) {
+        
+    }
+        
+    $("#addMember").click(function() {
+        if (!newMemberVisible) {        
+            $("#familyMemberList").prepend('<tr><td class="left"><img id="newPic" src="assets/unknown_person.jpg" /></td> <td class="middle"><input id="newName" type="text"></td> <td class="right"><button id="newAddButton" class="btn btn-success btn-mini">Add</btn></td></tr>');
+            newMemberVisible = true;        
+            $("#newAddButton").click(addMember);        
+        }
+    });         
 });
