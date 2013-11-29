bd32f31ba8ff4c967f3f37e49bc39159bf81668d
Sacha Zyto <sachazyto@gmail.com>
2013-11-29 00:03:51 -0500

    updated install instruction, and got rid of some non-needed grunt stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -466,7 +466,6 @@ module.exports = function(grunt) {
     });
     
 
-    grunt.loadNpmTasks('grunt-css');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
