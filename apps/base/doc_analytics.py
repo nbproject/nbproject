27421ca527aa2355420497f992f658be40b56058
Jen Liu <jkliu@mit.edu>
2013-12-23 00:47:35 -0500

    add num threads to page stats

--- apps/base/doc_analytics.py
+++ apps/base/doc_analytics.py
@@ -1,5 +1,6 @@
 import json
 from django.core import serializers
+import models as M
 
 def get_num_annotations_stats(sid):
 	import db
@@ -31,6 +32,20 @@ def get_num_questions_stats(sid):
 	"""
 	return db.Db().getIndexedObjects(attr, "page_num", from_clause, "true", [sid])
 
+def get_num_threads_stats(sid):
+	import db
+	attr = {
+		"page_num": "page",
+		"num_threads": None
+	}
+	from_clause = """
+	(SELECT page, count(*) as num_threads
+	FROM base_location
+	WHERE source_id = ?
+	GROUP BY page) as v1
+	"""
+	return db.Db().getIndexedObjects(attr, "page_num", from_clause, "true", [sid])
+
 def get_highlights(sid):
 	import db
 	attr = {
@@ -48,16 +63,37 @@ def get_highlights(sid):
 	"""
 	return db.Db().getIndexedObjects(attr, "id", from_clause, "true", [sid])
 
+def get_num_pages(sid):
+	numpages = M.Source.objects.get(id = sid).numpages
+	return numpages
+
 def get_page_stats(sid):
-	pages = get_num_annotations_stats(sid)
+	a_stats = get_num_annotations_stats(sid)
 	q_stats = get_num_questions_stats(sid)
-	for p in q_stats:
-		if p in pages:
-			pages[p]["num_questions"] = q_stats[p]["num_questions"]
+	t_stats = get_num_threads_stats(sid)
+
+	pages = {}
+	numpages = get_num_pages(sid)
+	for p in range(1, numpages+1):
+		if p in a_stats:
+			a_stat = a_stats[p]["num_annotations"]
 		else:
-			pages[p] = q_stats[p]
+			a_stat = 0
+		if p in q_stats:
+			q_stat = q_stats[p]["num_questions"]
+		else:
+			q_stat = 0
+		if p in t_stats:
+			t_stat = t_stats[p]["num_threads"]
+		else:
+			t_stat = 0
+		pages[p] = {
+			"page_num": p,
+			"num_threads": t_stat, 
+			"num_annotations": a_stat, 
+			"num_questions": q_stat
+		}
 	print pages
-	#return serializers.serialize("json", pages)
 	return json.dumps(pages)
 
 # def get_time_on_page(sid):
