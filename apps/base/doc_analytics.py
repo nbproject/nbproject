410687059bb5047758ad53515c609cc7e004b488
Jen Liu <jkliu@mit.edu>
2013-12-23 01:21:46 -0500

    basic column chart

--- apps/base/doc_analytics.py
+++ apps/base/doc_analytics.py
@@ -67,6 +67,11 @@ def get_num_pages(sid):
 	numpages = M.Source.objects.get(id = sid).numpages
 	return numpages
 
+# returns tuple (chart_stats, json_stats)
+# where chart_stats is an array of arrays: 
+#	[[page, num_threads, num_annotations, num_questions]]
+# and json_stats is a dictionary of dictionaries:
+#	{page_num: {page_dict}}
 def get_page_stats(sid):
 	a_stats = get_num_annotations_stats(sid)
 	q_stats = get_num_questions_stats(sid)
@@ -74,6 +79,7 @@ def get_page_stats(sid):
 
 	pages = {}
 	numpages = get_num_pages(sid)
+	chart_array = [["Page", "Threads", "Annotations", "Questions"]]
 	for p in range(1, numpages+1):
 		if p in a_stats:
 			a_stat = a_stats[p]["num_annotations"]
@@ -93,8 +99,11 @@ def get_page_stats(sid):
 			"num_annotations": a_stat, 
 			"num_questions": q_stat
 		}
-	print pages
-	return json.dumps(pages)
+		chart_array.append([p, int(t_stat), int(a_stat), int(q_stat)])
+
+	# print pages
+	print chart_array
+	return (json.dumps(pages), chart_array)
 
 # def get_time_on_page(sid):
 # 	SELECT page, ctime
