6f664b1c62b1bfdf5d47fdd9b372ee1b12f156f5
Jen Liu <jkliu@mit.edu>
2014-03-20 12:00:48 -0400

    time query works

--- apps/base/doc_analytics.py
+++ apps/base/doc_analytics.py
@@ -2,6 +2,7 @@ import json
 from django.core import serializers
 import models as M
 import datetime
+import psycopg2.extensions
 
 def get_num_annotations_stats(sid):
 	import db
@@ -71,7 +72,7 @@ def get_highlights(sid):
 	WHERE source_id = ?
 	AND type = 3) as v1
 	"""
-	print db.Db().getIndexedObjects(attr, "id", from_clause, "true", [sid])
+	# print db.Db().getIndexedObjects(attr, "id", from_clause, "true", [sid])
 	return db.Db().getIndexedObjects(attr, "id", from_clause, "true", [sid])
 
 def get_num_pages(sid):
@@ -137,7 +138,7 @@ def get_page_stats(sid):
 
 	# print pages
 	# print chart_array
-	# print get_time_stats(3343)
+	print get_time_stats(3343)
 	return (json.dumps(pages), chart_array)
 
 def markAnalyticsVisit(uid, o):
@@ -146,28 +147,53 @@ def markAnalyticsVisit(uid, o):
 		x = M.AnalyticsVisit(user_id=uid, source_id=id_source, ctime=datetime.datetime.fromtimestamp((o[id]+0.0)/1000))
 		x.save()
 
+import socket
+
+def test_connection():
+    """Test whether the postgres database is available. Usage:
+
+        if "--offline" in sys.argv:
+            os.environ['DJANGO_SETTINGS_MODULE'] = 'myapp.settings.offline'
+        else:
+            os.environ['DJANGO_SETTINGS_MODULE'] = 'myapp.settings.standard'
+            from myapp.functions.connection import test_connection
+            test_connection()
+    """
+    try:
+        s = socket.create_connection(("example.net", 5432), 5)
+        s.close()
+        print "connectiong established"
+    except socket.timeout:
+        msg = """Can't detect the postgres server. If you're outside the
+        intranet, you might need to turn the VPN on."""
+        print "err"
+        print socket.timeout(msg)
+
 def get_time_stats(sid):
 # conn = DB.getNewConnection()
 	from django.db import connection
 	import db
-
+	print "get time stats"
+	# print test_connection()
 	try:
+		print "in try"
 		c = connection.cursor()
+		print "cursor created"
 		qry = "drop table if exists nb2_page_order cascade;"
 		c.execute(qry)
 
+		print "executed"
 		qry ="create table nb2_page_order(id serial primary key, source_id integer, user_id integer, page integer, ctime timestamp, session_id integer);"
 		c.execute(qry)
-		
+		print "executed"
 		qry ="""		
 			insert into nb2_page_order(source_id, user_id, page, ctime,
 			session_id) select source_id, user_id, page, ctime, session_id
-			from base_pageseen where session_id is not null and source_id
-			in (select sid from v_ownership where eid= ?) order by
-			session_id, ctime;
+			from base_pageseen where session_id is not null and source_id= (%s) 
+			order by session_id, ctime;
 			"""
 		c.execute(qry, [sid])
-
+		print "executed"
 		qry = "DROP INDEX IF EXISTS nb2_page_order_id_session_idx ;"
 		c.execute(qry)
 
@@ -182,7 +208,7 @@ def get_time_stats(sid):
 
 		qry = "drop index if exists nb2_page_order_page_idx ;"
 		c.execute(qry)
-
+		print "executed"
 		qry = "CREATE INDEX nb2_page_order_id_session_idx ON nb2_page_order(session_id);"
 		c.execute(qry)
 
@@ -224,9 +250,12 @@ def get_time_stats(sid):
 			t.session_id=i.session_id and t.t1<i.t1 and t.t2>i.t2;
 			"""
 		c.execute(qry)
-	except:
-		print c.messages
+	except Exception, e:
+		print e.pgerror
+	finally:
+		c.close()
 
+	print "cursor stuff complete"
 	attr = {
 		"page": None,
 		"total_time": None
@@ -234,7 +263,7 @@ def get_time_stats(sid):
 	from_clause = """
 	(SELECT page, sum(tcorrected) as total_time from tpage2
 	WHERE source_id = ?
-	GROUP BY page;) as v1
+	GROUP BY page) as v1
 	"""
 	return db.Db().getIndexedObjects(attr, "page", from_clause, "true", [sid])
 
