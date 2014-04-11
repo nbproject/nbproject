ae3a8ed15bacde9da57c4157e2b298516db329df
Eyas <eyas.sharaiha@gmail.com>
2014-04-11 01:45:28 +0000

    Changed password reminder to password reset

--- apps/base/auth.py
+++ apps/base/auth.py
@@ -172,8 +172,11 @@ def user_from_email(email):
     return users[0] if len(users)==1 else None
          
 def checkUser(email, password):
-    users = M.User.objects.filter(email=email.strip().lower(), password=password, valid=1, guest=0)
-    return users[0] if len(users)==1 else None
+    users = M.User.objects.filter(email=email.strip().lower(), valid=1, guest=0)
+    if len(users) != 1:
+        return None
+    user = users[0]
+    return user if user.authenticate(password) else None
 
 def canAddFolder(uid, id_ensemble, id_parent=None): 
     return canInsertFile(uid, id_ensemble, id_parent)
