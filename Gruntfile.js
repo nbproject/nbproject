cdc92222c2f60b8d2165a7a62880ba0fa23aa0b1
Carolyn Zhang <czhang92@gmail.com>
2014-05-02 16:17:18 -0400

    Added grunt watch to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -467,9 +467,10 @@ module.exports = function(grunt) {
             }
         },
         watch: {
-            files: '<config:lint.files>',
-            tasks: 'lint qunit'
-        },        
+            files: ['<config:jshint.src>', 'content/ui/admin/*.css'],
+            tasks: ['jshint', 'concat', 'cssmin']
+
+        },
         uglify: {}
     });
     
@@ -477,7 +478,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     
     
