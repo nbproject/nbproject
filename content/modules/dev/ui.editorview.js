980b1679f524eb72cdef47bf77afd6e7b06f87f2
Sacha Zyto <sachazyto@gmail.com>
2015-08-10 03:12:43 -0400

    Title option only appears on new location

--- content/modules/dev/ui.editorview.js
+++ content/modules/dev/ui.editorview.js
@@ -164,7 +164,7 @@
                 var staffoption    = self._allowStaffOnly ? "<option value='2'>Instructors and TAs</option>" : " ";
                 var signoption    = self._allowAnonymous ? "<span id='signoption' title=\"check to keep this comment anonymous to other students\"><input type='checkbox' id='checkbox_sign' value='anonymous'/><label for='checkbox_sign'>Anonymous to students</label></div>": " ";
                 var questionoption = self._doEdit ? " " : "<span><input type='checkbox' id='checkbox_question' value='question'/><label for='checkbox_question'>Reply Requested</label></span><br/> ";
-                var titleoption = "<span><input type='checkbox' id='checkbox_title' value='title' /><label for='checkbox_question'>Is Section Title</label></span><br/> ";
+                var titleoption = self._note === null ? "<span><input type='checkbox' id='checkbox_title' value='title' /><label for='checkbox_question'>Is Section Title</label></span><br/> " : " ";
                 var checkbox_options = questionoption+titleoption+signoption;
                 var duration_option = model.o.file[self._file].filetype === FILETYPES.TYPE_YOUTUBE ? "<label for='duration'>Duration:</label><br/><input id='duration' type='text' size='1' value='2' /> seconds<br/>" : " ";
                 var header    = self._inReplyTo ? "Re: "+$.E($.ellipsis(self._note.body, 100)) : "New note...";
